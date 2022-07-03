d301f4a4562ff130047c1f98afdf57d43ffbb787
Ina <104801582+Ina-gencer@users.noreply.github.com>
2022-07-03 22:52:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import question from "./1.gif";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [par, setPar] = useState("");
+
+ useEffect(()=>{
+  getAdvice();
+ },[])
+
+ const getAdvice = async ()=> {
+  const responce = await fetch('http://www.boredapi.com/api/activity')
+  const data = await responce.json()
+  console.log(data.activity)
+  setAdvice(data.activity)
+  setPar(advice)
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">
+        <h1>Welcome to our fun app</h1>
+      </div>
+      <div className="App-container">
+        <p>Click on the button and find out what advice you got</p>
+      </div>
+      <div className="App-container">
+         <button onClick={getAdvice}>click here</button> 
+
+      </div>
+
+      <div className="App-container advice">
+        <h4>{par}</h4>
+        <img src={question} alt="smth move" width="50px" />
+      </div>
     </div>
   );
 }
